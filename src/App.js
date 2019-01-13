868f8eff8f06ffc66a08ad8015a94993aaac9344
Sanchit Bhatnagar <sanchitbhatnagar4@gmail.com>
2019-01-14 02:56:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,14 +7,11 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import ReduxThunk from 'redux-thunk';
-// import {
-//   Container
-// } from 'native-base';
+import {FirebaseApiKey,FirebaseSenderId} from './constants/key';
 
 import Router from './Router';
 import reducers from './reducers';
-// import LoginForm from './components/LoginForm';
-// import { Header } from './components/common';
+
 
 class App extends Component {
   componentWillMount() {
@@ -24,12 +21,12 @@ class App extends Component {
     const firebase = require('firebase');
 
     const config = {
-      apiKey: 'AIzaSyD-_h6CybTyGEx47VN2g9OrhMFR-ZdDKEo',
+      apiKey: FirebaseApiKey,
       authDomain: 'managerreactnativeapp.firebaseapp.com',
       databaseURL: 'https://managerreactnativeapp.firebaseio.com',
       projectId: 'managerreactnativeapp',
       storageBucket: 'managerreactnativeapp.appspot.com',
-      messagingSenderId: '643912206745'
+      messagingSenderId: FirebaseSenderId
     };
     firebase.initializeApp(config);
   }
@@ -37,11 +34,8 @@ class App extends Component {
     const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
     return (
       <Provider store={store}>
-        {/* <Container> */}
-        {/* <Header headerText='Login Page' /> */}
-        {/* <LoginForm /> */}
+
         <Router />
-        {/* </Container> */}
       </Provider>
     );
   }
